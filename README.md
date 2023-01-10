af984b6b02bac8171867fa545e75ccf9283dde59
vishruth10 <83918807+vishruth10@users.noreply.github.com>
2023-01-10 22:59:25 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# node-tprxys
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/node-t5izvt)
---
+++ index.js
@@ -0,0 +1,11 @@
+for (let i = 1; i < 101; i++) {
+  if(i%5==0 && i%3==0)
+  document.write("amazon");
+  else if(i%5==0)
+  document.write("google");
+  else if(i%3==0)
+  document.write("facebook");
+  else 
+  document.write(i);
+  
+}
